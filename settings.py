5f1717c3ef9eae2d28b70c7fd277084b63fe3175
bva24 <anandsastry24@gmail.com>
2015-04-29 23:43:46 +0530

    'changed the TEMPALTE CONTEXT PROCESSOR'

--- settings.py
+++ settings.py
@@ -340,6 +340,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.request",
     "django.core.context_processors.tz",
     "mezzanine.conf.context_processors.settings",
+    "mezzanine.pages.context_processors.page", #Added by me as shown in CMD while starting local server
 )
 
 # List of middleware classes to use. Order is important; in the request phase,
